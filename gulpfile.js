57110e723787aa964baf4266d6f37fc0192db93e
Hazel Wu <tinazx056@gmail.com>
2017-11-07 00:45:29 +0800

    install bower & task: vendorJs, bowser

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,21 @@
 // dependencies
 var gulp          = require('gulp'),
-    imagemin      = require('gulp-imagemin'),
-    pngquant      = require('imagemin-pngquant'), //深度壓縮png
-    uglify        = require('gulp-uglify'),
-    sass          = require('gulp-sass'),
-    concat        = require('gulp-concat'),
-    watch         = require('gulp-watch'),
-    cleanCSS      = require('gulp-clean-css'),
     browserSync   = require('browser-sync').create(),
-    plumber       = require('gulp-plumber'),
-    postcss       = require('gulp-postcss'),
+    pngquant      = require('imagemin-pngquant'), //深度壓縮png
     autoprefixer  = require('autoprefixer'),
-    sourcemaps    = require('gulp-sourcemaps'), //來源地圖
-    changed       = require('gulp-changed'),    //有修改過的文件才會變更
-    gutil         = require('gulp-util'), //上顏色console.log
-    colors        = gutil.colors;
+    mainBowerFiles = require('main-bower-files'),
+    $             = require('gulp-load-plugins')();
+
 
 // Logs Message
 gulp.task('message', function(){
-  return console.log('\n'+ colors.red('Gulp 正常運作中...'))
+  return console.log('\n'+ 'Gulp 正常運作中...')
 });
 
 // Copy ALL HTML files
 gulp.task('copyHtml', function(){
   gulp.src('src/**/*.html')
-      .pipe(plumber())
+      .pipe($.plumber())
       .pipe(gulp.dest('public'))
       .pipe(browserSync.stream()); //自動重新整理
   return console.log('任務「CopyHtml」已完成，請到dist資料夾查看。');
@@ -33,9 +24,9 @@ gulp.task('copyHtml', function(){
 // Optimize Images 優化圖片
 gulp.task('imageMin', function(){
   gulp.src('src/images/*')
-    .pipe(plumber())
-    .pipe(changed('./public/images'))
-    .pipe(imagemin({
+    .pipe($.plumber())
+    .pipe($.changed('./public/images'))
+    .pipe($.imagemin({
       progressive: true, //無損壓縮jpg圖
       use: [pngquant()] //深度壓縮PNG
     }))
@@ -50,9 +41,9 @@ gulp.task('sass',function(){
         autoprefixer({browsers: ['last 3 version']})
   ];
   gulp.src(['src/sass/*.sass','src/sass/*.scss'])
-      .pipe(plumber())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(plugins))
+      .pipe($.plumber())
+      .pipe($.sass().on('error', $.sass.logError))
+      .pipe($.postcss(plugins))
       // 編譯完成 CSS
       .pipe(gulp.dest('./public/css'))
       .pipe(browserSync.stream())
@@ -62,9 +53,9 @@ gulp.task('sass',function(){
 // Conpile css
 gulp.task('css', function(){
   gulp.src(['src/css/bootstrap.css','src/css/style.css','src/css/responsive.css'])
-      .pipe(plumber())
-      .pipe(cleanCSS())
-      .pipe(concat('style.css'))
+      .pipe($.plumber())
+      .pipe($.cleanCss())
+      .pipe($.concat('style.css'))
       .pipe(gulp.dest('public/css'))
       .pipe(browserSync.stream());
   return console.log('任務「 Minify CSS 」，mini CSS已打包完成。');
@@ -72,16 +63,30 @@ gulp.task('css', function(){
 
 // Scripts 打包成一個main.js
 gulp.task('scripts', function(){
-  gulp.src(["src/js/jquery-1.9.1.min.js","src/js/**/*.js"])
-      .pipe(plumber())
-      .pipe(concat('main.js'))
-      .pipe(uglify())
-      .pipe(sourcemaps.write('maps'))
+  gulp.src(["src/js/**/*.js"])
+      .pipe($.plumber())
+      .pipe($.concat('main.js'))
+      .pipe($.uglify())
+      .pipe($.sourcemaps.write('maps'))
       .pipe(gulp.dest('./public/js'))
       .pipe(browserSync.stream());
   return console.log('任務「 Scripts 」，所有js壓縮並匯集成main.js，已打包完成。');
 });
 
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulp.dest('./vendors/'))
+});
+
+gulp.task('vendorJs', ['bower'], function(){
+  return gulp.src(['./vendors/**/*.js'])
+  .pipe($.order([
+        'jquery.js',
+        'bootstrap.js'
+        ]))
+  .pipe($.concat('vendors.js'))
+  .pipe(gulp.dest('./public/js'))
+})
 
 // Static server
 gulp.task('browserSync', function() {
@@ -93,7 +98,7 @@ gulp.task('browserSync', function() {
 });
 
 // Default task
-gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'css','scripts','browserSync', 'watch'])
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'css','scripts','vendorJs' ,'browserSync', 'watch'])
 
 
 
